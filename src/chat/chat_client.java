e817156590d194d6cb68e2e80a88e355c8a35e06
Apollo13 <calderon.ig08@estudiantec.cr>
2020-09-14 18:45:44 -0600

    create client frame

---
+++ src/chat/chat_client.java
@@ -0,0 +1,34 @@
+package chat;
+ // comentar
+import javax.swing.*;
+import java.awt.*;
+
+public class chat_client {
+
+    public static void main (String[] args) {
+
+        Frames screenC = new Frames();
+        // This method shows the frame
+        screenC.setVisible(true);
+        // This method set the tittle
+        screenC.setTitle("Client");
+        // This method set the background color
+        //screenC.setBackground(Color.gray);
+        // This method stops the app
+        screenC.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+class Frames extends JFrame{
+
+    // Variables for screen size
+    int Fwidth = 350;
+    int Fheight = 700;
+    Dimension screen_dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+    public Frames(){
+        //System.out.println(screen_dim);
+        setSize(Fwidth, Fheight);
+        setLocation(305, 190);
+    }
+}
