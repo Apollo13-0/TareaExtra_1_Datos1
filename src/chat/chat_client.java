976438c73bc46c60b93b64dd4bedf6b61943eb23
Apollo13 <calderon.ig08@estudiantec.cr>
2020-09-17 21:55:22 -0600

    GUI client finish

--- src/chat/chat_client.java
+++ src/chat/chat_client.java
@@ -7,51 +7,65 @@ public class chat_client {
 
     public static void main (String[] args) {
 
-        //
-        int Fwidth = 350;
-        int Fheight = 700;
-
-        //
-        JFrame screenC = new JFrame("Client");
-
-        // This method set the size of the frame
-        screenC.setSize(Fwidth, Fheight);
-        screenC.setLocation(305, 190);
-        // This method don't allow to change frame dimensions
-        screenC.setResizable(false);
-        // This method shows the frame
-
-        // This method set the background color
-        screenC.getContentPane().setBackground(new Color(236,229,221));
-        // This method stops the app
-        screenC.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Panel
-        JPanel panelC = new JPanel();
-        //panelC.setSize(350, 50);
-        panelC.setBackground(new Color(18,140,128));
-
-        JButton send = new JButton("Enviar");
-        JTextField tf = new JTextField(20);
-
-        JPanel panelT = new JPanel();
-        panelT.setBackground(new Color(7,94,84));
-        JLabel labelT = new JLabel();
-        labelT.setText("Servdor");
-        labelT.setForeground(new Color(250,250,250));
-
-        panelT.add(labelT);
-
-
-        panelC.add(tf);
-        panelC.add(send);
-
-
-        screenC.getContentPane().add(BorderLayout.SOUTH, panelC);
-        screenC.getContentPane().add(BorderLayout.NORTH, panelT);
-        screenC.setVisible(true);
-
+    Frames clientFrame = new Frames();
+    //This method stops the app
+    clientFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 }
 
+class Frames extends JFrame{
+
+    public Frames(){
+
+        //
+        short Fwidth = 350;
+        short Fheight = 700;
+
+        // This method set the size of the frame
+        setSize(Fwidth, Fheight);
+        setLocation(305, 190);
+        // This method don't allow to change frame dimensions
+        setResizable(false);
+        // This method shows the frame
+        setTitle("Client");
+
+        // This method set the background color
+        getContentPane().setBackground(new Color(236,229,221));
+
+        PanelClient containerClient = new PanelClient();
+        add(containerClient, BorderLayout.SOUTH);
+
+        JPanel title = new JPanel();
+        JLabel titleTxt = new JLabel("Servidor");
+        title.setBackground(new Color(7,94,84));
+        titleTxt.setForeground(new Color(255,255,255));
+        title.add(titleTxt);
+        add(title, BorderLayout.NORTH);
+
+        setVisible(true);
+
+    }
+}
+
+class PanelClient extends JPanel{
+
+    private JTextField entryTxtClient;
+    private JButton sendClient;
+
+    public PanelClient() {
+
+        // This method set the background color
+        setBackground(new Color(18, 140, 128));
+
+        entryTxtClient = new JTextField(20);
+
+        add(entryTxtClient, BorderLayout.CENTER);
+
+        sendClient = new JButton("Enviar");
+
+        add(sendClient, BorderLayout.SOUTH);
+
+    }
+
+}
